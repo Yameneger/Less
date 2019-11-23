7d964fd3a8e52ca99e1415fdef74fe62da6517a5
Yameneger <58104923+Yameneger@users.noreply.github.com>
2019-11-23 13:56:25 +0200

    Create script 01_os_info

---
+++ 01_os_info.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+
+
+import ...
+
+info = 'OS info is \n{} \n\nPython version is {}  {}'.format(
+    platform.uname(), sys.version, platform.architecture())
+print (info)
+
+with open('os_info.txt', 'w') as ff:
+    ff.write(info)
